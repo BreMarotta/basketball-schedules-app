8d7f7cb7660b990666e074c9dd9b0ef8b4bd9a37
Breanne <BreMarotta@gmail.com>
2022-02-26 06:23:50 -0500

    move Location fetch out of context

--- src/Location.js
+++ src/Location.js
@@ -1,23 +1,38 @@
-import React, { useState } from 'react' 
-import { MyConsumer } from './MyContext'
+import React, { useState, useEffect } from 'react' 
 import AddLocationButton from './AddLocationButton'
 
 const Location = () => {
+    const [locations, setLocations] = useState([])
+     
+    useEffect(() => {
+        fetch('http://localhost:3004/locations')
+        .then(res => res.json())
+        .then(data => setLocations(data))
+    }, [])
 
+    const allLocations = locations.map(l => {
+        return (
+        <div key={l.id}>
+            <h3 style={{marginLeft: "5px"}}>{l.name}</h3>
+            <p style={{marginLeft: "25px"}}>Address: {l.address1}</p>
+            <p style={{marginLeft: "95px"}}>  {l.address2}</p>
+            <hr />
+        </div>
+        )
+    })
+    
+    const handleLocationSubmit = (e) => {
+        e.preventDefault()
+        console.log(e)
+    }
    
 
     return(
         <div>
             <h1>Hello from Location Page</h1>
         <AddLocationButton />
-        <MyConsumer>
-            {context => 
-            <div>
-                <div>{context.allLocations}</div>
-            </div>
-            }
-            
-        </MyConsumer>
+        {allLocations}
+        
         </div>
     )
 }
--- src/MyContext.js
+++ src/MyContext.js
@@ -97,29 +97,7 @@ const MyProvider = (props) => {
         })
     }, [])
  
-    const [locations, setLocations] = useState([])
-     
-    useEffect(() => {
-        fetch('http://localhost:3004/locations')
-        .then(res => res.json())
-        .then(data => setLocations(data))
-    }, [])
-
-    const allLocations = locations.map(l => {
-        return (
-        <div key={l.id}>
-            <h3 style={{marginLeft: "5px"}}>{l.name}</h3>
-            <p style={{marginLeft: "25px"}}>Address: {l.address1}</p>
-            <p style={{marginLeft: "95px"}}>  {l.address2}</p>
-            <hr />
-        </div>
-        )
-    })
     
-    const handleLocationSubmit = (e) => {
-        e.preventDefault()
-        console.log(e)
-    }
 
     // const filter = (teamName) => {
     //     const filteredGames = allGames.filter(g => {
@@ -143,9 +121,7 @@ const MyProvider = (props) => {
         <MyContext.Provider value={{
             allGames: allGames,
             teams: teams,
-            allLocations: allLocations,
             games: games,
-            handleLocationSubmit: handleLocationSubmit
         }}>
             {props.children}
         </MyContext.Provider>
